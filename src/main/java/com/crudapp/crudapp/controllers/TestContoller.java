3aac71314563daa0dc10991a5f17333100b71de0
NikitaDyagilev <nikita.novikov03@gmail.com>
2020-04-22 23:37:53 -0500

    second commit

---
+++ src/main/java/com/crudapp/crudapp/controllers/TestContoller.java
@@ -0,0 +1,17 @@
+package com.crudapp.crudapp.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class TestContoller {
+
+	@RequestMapping(value="/sayHello", method=RequestMethod.GET)
+	public ResponseEntity<String> sayHello() {
+		return ResponseEntity.ok().body("Hello World!");
+	}
+	
+}
---
+++ src/main/java/com/crudapp/crudapp/model/Account.java
@@ -0,0 +1,35 @@
+package com.crudapp.crudapp.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Table
+@Data
+public class Account {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long accountID;
+    
+    @Column(name = "first_name")
+    private String first_name;
+
+    @Column(name = "last_name")
+    private String last_name;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "bio")
+    private String bio;
+
+    
+
+
+}
